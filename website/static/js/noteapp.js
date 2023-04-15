43b2bdd751e12c29e64394e0ef5ae49c43ad25a1
Kennedy R. Ngigi <kennedyngigi4@gmail.com>
2023-04-15 07:45:10 +0300

    Initial commit

---
+++ website/static/js/noteapp.js
@@ -0,0 +1,10 @@
+console.log('My js ...')
+
+function deleteNote(noteId){
+    fetch('/delete-note', {
+        method: 'POST',
+        body: JSON.stringify({ noteId: noteId})
+    }).then((_res) => {
+        window.location.href = "/"
+    })
+}
